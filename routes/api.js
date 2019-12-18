6e7c5fbd81c67e1bf6bbda83f515b1fe9546da9e
ihsanhusaeri <50247104+ihsanhusaeri@users.noreply.github.com>
2019-12-18 17:39:10 +0700

    Update api.js
    Testing

--- routes/api.js
+++ routes/api.js
@@ -30,4 +30,5 @@
         |--------------------------------------------------------------------------
         */
         app.get( '/api/v1.0/push-kafka', Controller.ExportKafka.pushKafka );
+     app.get( '/api/v1.0/push-kafka2', Controller.ExportKafka.pushKafka );
     }
