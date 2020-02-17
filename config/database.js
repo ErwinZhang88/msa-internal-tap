a47fe809f4ea5e14a64f4cd47974b2e320babd72
ihsanhusaeri <ihusaeri@gmail.com>
2020-02-17 10:32:23 +0700

    tambah database oracle QA

--- config/database.js
+++ config/database.js
@@ -4,6 +4,11 @@ module.exports = {
 		password      : process.env.NODE_ORACLEDB_PASSWORD || "mobile_inspection",
 		connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "10.20.1.111:1521/tapapps"
 	},
+	qa: {
+		user          : process.env.NODE_ORACLEDB_USER || "mobile_inspection",
+		password      : process.env.NODE_ORACLEDB_PASSWORD || "mobile_inspection",
+		connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "10.20.1.111:1521/tapapps"
+	},
 	prod: {
 		user          : process.env.NODE_ORACLEDB_USER || "mobile_inspection",
 		password      : process.env.NODE_ORACLEDB_PASSWORD || "mobile_inspection",
