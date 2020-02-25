4f3a7b24cf79d823e3572f323a92badf337158a7
ihsanhusaeri <ihusaeri@gmail.com>
2020-02-25 14:09:28 +0700

    ubah env ke prod

--- config/app.js
+++ config/app.js
@@ -11,7 +11,7 @@
 		|--------------------------------------------------------------------------
 		*/
 			name: 'Microservice Internal TAP',
-			env: 'qa', // prod, qa, dev,
+			env: 'prod', // prod, qa, dev,
 			port: {
 				dev: process.env.PORT || 4015,
 				qa: process.env.PORT || 5015,
