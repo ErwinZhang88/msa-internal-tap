0f46de55152758e3e98aa489c0603dab5d946d61
ihsanhusaeri <ihusaeri@gmail.com>
2020-05-22 15:35:25 +0700

    tambah config untuk prod

--- config/database.js
+++ config/database.js
@@ -21,14 +21,28 @@
 			},
 		},
 		qa: {
-			user          : process.env.NODE_ORACLEDB_USER || "mobile_inspection",
-			password      : process.env.NODE_ORACLEDB_PASSWORD || "mobile_inspection",
-			connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "10.20.1.111:1521/tapapps"
+			mobileInspection: {
+				user          : process.env.DB_USER_MI, 
+				password      : process.env.DB_PASSWORD_MI,
+				connectString : process.env.DB_URL_QA
+			},
+			patroliApi: {
+				user          : process.env.DB_USER_PATROLI,
+				password      : process.env.DB_PASSWORD_PATROLI, 
+				connectString : process.env.DB_URL_QA
+			},
 		},
 		prod: {
-			user          : process.env.NODE_ORACLEDB_USER || "mobile_inspection",
-			password      : process.env.NODE_ORACLEDB_PASSWORD || "mobile_inspection",
-			connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "10.20.1.207:1521/tapapps"
+			mobileInspection: {
+				user          : process.env.DB_USER_MI, 
+				password      : process.env.DB_PASSWORD_MI,
+				connectString : process.env.DB_URL_PROD
+			},
+			patroliApi: {
+				user          : process.env.DB_USER_PATROLI,
+				password      : process.env.DB_PASSWORD_PATROLI, 
+				connectString : process.env.DB_URL_PROD
+			}
 		}
 	};
 
